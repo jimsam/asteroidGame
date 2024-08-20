431c32105f350bc192c25186a210cb0d3b53e2ab
Jim Samios <dsamios@hey.com>
2024-08-20 12:45:17 -0500

    Main.

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import pygame
 
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 from constants import *
+from player import Player
+from shot import Shot
 
 
 def main():
@@ -8,14 +12,38 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     running = True
+    dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shoots = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    Shot.containers = (shoots, updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroidField = AsteroidField()
 
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        pygame.display.flip()
 
-        clock.tick(60)
+        dt = clock.tick(60) / 1000
+        screen.fill("black")
+        for item in updatable:
+            item.update(dt)
+        for item in asteroids:
+            if player.collition_check(item):
+                print("Game Over!")
+                running = False
+            for bullet in shoots:
+                if bullet.collition_check(item):
+                    bullet.kill()
+                    item.split()
+        for item in drawable:
+            item.draw(screen)
+        pygame.display.flip()
     pygame.quit()
 
 
