346130aa50865b9e160ae0ba0248ee6c7b13c57d
Jim Samios <dsamios@hey.com>
2024-08-20 12:45:10 -0500

    Shoting bullets.

---
+++ shot.py
@@ -0,0 +1,20 @@
+import pygame
+
+from circleshape import CircleShape
+from constants import SHOT_RADIUS
+
+
+class Shot(CircleShape):
+    def __init__(self, x, y, radius=SHOT_RADIUS):
+        super().__init__(x, y, radius)
+        self.position = pygame.Vector2(x, y)
+        self.rotation = 0
+        self.velocity = 0
+
+    def draw(self, screen):
+        pygame.draw.circle(
+            screen, "yellow", pygame.Vector2(self.position), SHOT_RADIUS, 2
+        )
+
+    def update(self, dt):
+        self.position += self.velocity * dt
