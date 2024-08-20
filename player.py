80e12d75b140b407026ec8c0bd47bdb239980004
Jim Samios <dsamios@hey.com>
2024-08-20 12:43:55 -0500

    Player code.

---
+++ player.py
@@ -0,0 +1,62 @@
+import pygame
+
+from circleshape import CircleShape
+from constants import (
+    PLAYER_RADIUS,
+    PLAYER_SHOOT_SPEED,
+    PLAYER_SHOOTING_RATE,
+    PLAYER_SPEED,
+    PLAYER_TURN_SPEED,
+)
+from shot import Shot
+
+
+class Player(CircleShape):
+    def __init__(self, x, y):
+        super().__init__(x, y, PLAYER_RADIUS)
+        self.position = pygame.Vector2(x, y)
+        self.rotation = 0
+        self.shooting_cd = 0
+
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+
+    def draw(self, screen):
+        pygame.draw.polygon(screen, "white", self.triangle(), 2)
+
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+        if self.shooting_cd > -1:
+            self.shooting_cd -= dt
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        elif keys[pygame.K_d]:
+            self.rotate(dt)
+        elif keys[pygame.K_w]:
+            self.move(dt)
+        elif keys[pygame.K_s]:
+            self.move(-dt)
+        elif keys[pygame.K_SPACE]:
+            self.shoot()
+
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
+
+    def shoot(self):
+        if self.shooting_cd > 0:
+            return
+        self.shooting_cd = PLAYER_SHOOTING_RATE
+        bullet = Shot(self.position.x, self.position.y)
+        bullet.velocity = (
+            pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+        )
